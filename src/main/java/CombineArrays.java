e6ea5aa1eebd0f5eb2d40e47030761f68d5dd783
theo ferguson <fergy546@yahoo.com>
2023-03-05 19:25:43 +0000

    complete

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int[] solution = new int[arr1.length + arr2.length];
 
-        return null;
+        for (int i = 0; i < arr1.length; i++) {
+            solution[i] = arr1[i];
+        }
+        for (int j = arr1.length; j < solution.length; j++) {
+            solution[j] = arr2[j - arr1.length];
+        }
+
+        return solution;
+        
     }
 }
